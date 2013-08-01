a5337021459a9ec9490892f2b0e75acb603f4fb7
Lyric P Doshi <lyric@mit.edu>
2013-07-31 22:28:02 -0400

    hiy

--- src/tz/co/shuledirect/machineSetup/setup.sh
+++ src/tz/co/shuledirect/machineSetup/setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install apache2
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
-sudo apt-get install php5
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
 sudo apt-get install emacs
 sudo chown -R ubuntu /var/www
 sudo ln -s /home/ubuntu/shule-internal/src/tz/co/shuledirect/api/ /var/www
