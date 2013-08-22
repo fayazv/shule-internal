daf7aeea554e901abd2ccaf78246fb83dd9824d0
Ramkrishnamurth Bhaskaramurthi <rambhask@mit.edu>
2013-08-21 22:02:22 -0400

    added skeleton methods

--- src/tz/co/shuledirect/api/application/controllers/notes.php
+++ src/tz/co/shuledirect/api/application/controllers/notes.php
@@ -5,7 +5,7 @@ require APPPATH.'/libraries/REST_Controller.php';
 
 class Notes extends REST_Controller
 {
-    function getAugmentedNotes_get()
+    function getAugmentedNotes_get($subjectId)
     {
     	$this->load->model('notes');
     	$test = $this->notes->getAugmentedNotes();
@@ -16,4 +16,43 @@ class Notes extends REST_Controller
 	 $this->response($array);	
     }
 
+    function setAugmentedNotes($subjectId, $newContent)
+    {
+    	
+    }
+
+    function addContent($parentId, $newContent)
+    {
+
+    }
+
+    function editContent($id, $editedContent)
+    {
+
+    }
+
+    function deleteContent($id)
+    {
+
+    }
+
+    function addTag($parentId, $newTag)
+    {
+
+    }
+
+    function deleteTag($parentId, $tagId)
+    {
+
+    }
+
+    function addMedia($parentId, $newContent, $type, $description, $isPrintable)
+    {
+
+    }
+
+    function deleteMedia($parentId, $mediaId)
+    {
+
+    }
 }
