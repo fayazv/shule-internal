189ead543ba4e040be9f198d81bb9aaeea47992f
Ramkrishnamurth Bhaskaramurthi <rambhask@mit.edu>
2013-08-23 21:42:31 -0400

    added updates to controller and model

--- src/tz/co/shuledirect/api/application/controllers/notes.php
+++ src/tz/co/shuledirect/api/application/controllers/notes.php
@@ -3,6 +3,9 @@
 
 require APPPATH.'/libraries/REST_Controller.php';
 
+// So the controller will always recieve a string that it will parse and send contents to the model. 
+// The model will then take these values and make appropriate db calls. 
+
 class Notes extends REST_Controller
 {
     public function __construct()
@@ -11,51 +14,78 @@ class Notes extends REST_Controller
         $this->load->model('Notes_m');
     }
 
-    function getAugmentedNotes_get()
+    function validateInt($int)
     {
-    	$test = $this->Notes_m->getAugmentedNotes();
-        $data["foo"] = $test;
-        $data["bar"] = "lalala";
-	$this->response($data);	
+        if(!filter_var($int, FILTER_VALIDATE_INT))
+        {
+            echo("Integer is not valid");
+        }
+        else
+        {
+            echo("Integer is valid");
+        }
     }
 
-    function setAugmentedNotes($subjectId, $newContent)
+    function getAugmentedNotes_get($inputJson)
     {
-    	
+    	$object = json_decode($inputJson, true);
+        if (array_key_exists("id", $object))
+        {
+            $subjectId = $object["id"];
+        }
+        validateInt($subjectId);
+
+        $augmentedNotesObject = $this->Notes_m->getAugmentedNotes($subjectId);
+	    $this->response($augmentedNotesObject);	
     }
 
-    function addContent($parentId, $newContent)
+    function setAugmentedNotes($inputJson)
     {
+    	$object = json_decode($inputJson, true);
+        if (array_key_exists("id", $object) && array_key_exists("content", $object))
+        {
+            $subjectId = $object["id"];
+            $newContent = $object["content"];
+        }
 
+        validateInt($subjectId);
+        $success = $this->Notes_m->setAugmentedNotes($subjectId, $newContent); 
+        //success is a boolean
+        $this->response($success);
     }
 
-    function editContent($id, $editedContent)
+    function addContent($inputJson)
     {
-
+        $object = json_decode($inputJson, true);
     }
 
-    function deleteContent($id)
+    function editContent($inputJson)
     {
-
+        $object = json_decode($inputJson, true);
     }
 
-    function addTag($parentId, $newTag)
+    function deleteContent($inputJson)
     {
-
+        $object = json_decode($inputJson, true);
     }
 
-    function deleteTag($parentId, $tagId)
+    function addTag($inputJson)
     {
-
+        $object = json_decode($inputJson, true);
     }
 
-    function addMedia($parentId, $newContent, $type, $description, $isPrintable)
+    function deleteTag($inputJson)
     {
-
+        $object = json_decode($inputJson, true);
     }
 
-    function deleteMedia($parentId, $mediaId)
+    function addMedia($inputJson)
     {
+        $object = json_decode($inputJson, true);
+    }
 
+    function deleteMedia($inputJson)
+    {
+        $object = json_decode($inputJson, true);
     }
 }
--- src/tz/co/shuledirect/api/application/models/notes_m.php
+++ src/tz/co/shuledirect/api/application/models/notes_m.php
@@ -7,9 +7,57 @@ class Notes_m extends CI_Model {
         parent::__construct();
     }
 
-    function getAugmentedNotes()
+    function getAugmentedNotes($subjectId)
     {
-	return "frustration";
+    	return "TODO: implement this method";
+    }
+    
+
+    function setAugmentedNotes($subjectId, $newContent)
+    {
+		return "TODO: implement this method";
+    }
+
+
+    function addContent($parentId,$newContent)
+    {
+    	return "TODO: implement this method";
+    }
+   
+
+    function editContent($id,$editedContent)
+    {
+    	return "TODO: implement this method";
+    }
+
+
+    function deleteContent($id)
+    {
+    	return "TODO: implement this method";
+    }
+
+
+    function addTag($parentId, $newTag)
+    {
+    	return "TODO: implement this method";
+    }
+
+
+    function deleteTag($parentId, $tagId)
+    {
+    	return "TODO: implement this method";
+    }
+
+
+    function addMedia($parentId, $newContent, $type, $description, $isPrintable)
+    {
+    	return "TODO: implement this method";
+    }
+
+
+    function deleteMedia($parentId, $mediaId)
+    {
+    	return "TODO: implement this method";
     }
 }
 
