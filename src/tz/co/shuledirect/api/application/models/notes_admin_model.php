0aa50cb7d13116d26e0c0313140ae8aa806fd5fe
ldoshi <ldoshi@companion-cube.(none)>
2013-10-07 02:21:18 -0400

    getId works, remove old models

--- src/tz/co/shuledirect/api/application/models/notes_admin_model.php
+++ src/tz/co/shuledirect/api/application/models/notes_admin_model.php
@@ -28,13 +28,15 @@ class Notes_admin_model extends CI_Model {
         // TODO handle database errors
         // error also if try add a child to a paragraph tier which does not support children. 
         $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
-        $note_type_id_query = $this->db->query("select id from note_types AS note_types_one where note_types_one.depth = (select (note_types_two.depth+1) as new_depth from notes JOIN note_types AS note_types_two ON notes.note_type_id = note_types_two.id where notes.id = $parentId);");
-        $note_type_id_row = $note_type_id_query->row();
-        $note_type_id = $note_type_id_row->id; 
-        $position_query = $this->db->query("select CASE WHEN max(position)+1 IS NOT NULL THEN (max(position)+1) ELSE 0 END as position from notes where parent_notes_id = $parentId;");
-        $position_row = $position_query->row();
-        $position = $position_row->position;
-        $insert = $this->db->query("INSERT INTO notes (content,position,note_type_id,parent_notes_id,language_id) VALUES (".$this->db->escape($newContent).",$position,$note_type_id,$parentId,$this->englishLanguageId);");
+        $noteTypeIdQuery = $this->db->query("select id from note_types AS note_types_one where note_types_one.depth = (select (note_types_two.depth+1) as new_depth from notes JOIN note_types AS note_types_two ON notes.note_type_id = note_types_two.id where notes.id = $parentId);");
+        $noteTypeIdRow = $noteTypeIdQuery->row();
+        $noteTypeId = $noteTypeIdRow->id; 
+
+        $positionQuery = $this->db->query("select CASE WHEN max(position)+1 IS NOT NULL THEN (max(position)+1) ELSE 0 END as position from notes where parent_notes_id = $parentId;");
+        $positionRow = $positionQuery->row();
+
+        $position = $positionRow->position;
+        $insert = $this->db->query("INSERT INTO notes (content,position,note_type_id,parent_notes_id,language_id) VALUES (".$this->db->escape($newContent).",$position,$noteTypeId,$parentId,$this->englishLanguageId);");
         $this->db->query("COMMIT;");
         return true;
     }
--- src/tz/co/shuledirect/api/application/models/notes_model.php
+++ src/tz/co/shuledirect/api/application/models/notes_model.php
@@ -19,9 +19,59 @@ class Notes_model extends CI_Model {
     
     public function getId($form = NULL, $subject = NULL, $topic = NULL, $subtopic = NULL, $concept = NULL)
     {
-        //Implement this!
-        $form = NULL;
-        return true;
+        $project = "ShuleDirect";
+
+        // build up the WHERE and JOIN clauses depending on how many parameters are provided. The params provided also determine which id to return 
+        $whereClause = "WHERE project.content = ".$this->db->escape($project)." AND project.parent_notes_id IS NULL";
+        $joinClause = "";
+        $returnId = "project.id"; // defaults to project, may be overwritten
+        
+        if($form != NULL) {
+            $joinClause .= " JOIN notes form ON project.id = form.parent_notes_id";
+            $whereClause .= " AND form.content = ".$this->db->escape($form)." ";
+
+            if($subject != NULL) {
+                $joinClause .= " JOIN notes subject ON form.id = subject.parent_notes_id";
+                $whereClause .= " AND subject.content = ".$this->db->escape($subject)." ";
+
+                if($topic != NULL) {
+                    $joinClause .= " JOIN notes topic ON subject.id = topic.parent_notes_id";
+                    $whereClause .= " AND topic.content = ".$this->db->escape($subject)." ";
+
+                    if($subtopic != NULL) {
+                        $joinClause .= " JOIN notes subtopic ON topic.id = subtopic.parent_notes_id";
+                        $whereClause .= " AND subtopic.content = ".$this->db->escape($subtopic)." ";
+                        
+                        if($concept != NULL) {
+                            $joinClause .= " JOIN notes concept ON subtopic.id = concept.parent_notes_id";
+                            $whereClause .= " AND concept.content = ".$this->db->escape($concept)." ";
+                            $returnId = "concept.id";
+                        } else {
+                            $returnId = "subtopic.id";
+                            // and we're done with the params
+                        }
+                    } else {
+                        $returnId = "topic.id";
+                        // and we're done with the params
+                    }                    
+                } else {
+                    $returnId = "subject.id";
+                    // and we're done with the params
+                }
+            } else {
+                $returnId = "form.id";
+                // and we're done with the params
+            }
+        } 
+
+        $selectIdQuery = "SELECT $returnId FROM notes AS project $joinClause $whereClause;";
+        $idQuery = $this->db->query($selectIdQuery);
+        if($idRow = $idQuery->row()) {
+            return $idRow->id;
+        } else {
+            // TODO whats the correct 'not found' handling?
+            return 0;
+        }
     }
     
 }
