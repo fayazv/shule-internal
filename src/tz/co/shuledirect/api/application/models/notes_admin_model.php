37c4026cfb9eec25a46f2e388f401d188d42160e
Ubuntu <ubuntu@ip-172-31-4-249.us-west-2.compute.internal>
2013-10-12 19:13:50 +0000

    comprehensive test 1 complete, cleaned up code

--- src/tz/co/shuledirect/api/application/models/notes_admin_model.php
+++ src/tz/co/shuledirect/api/application/models/notes_admin_model.php
@@ -28,6 +28,7 @@ class Notes_admin_model extends CI_Model {
         // TODO handle database errors
         // TODO should all the queries need to check if numRow() > 0?
         // error also if try add a child to a paragraph tier which does not support children. 
+        //TODO: The line below should NOT be here. Dunno why it is required.
         $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
         $noteTypeIdQuery = $this->db->query("select id from note_types AS note_types_one where note_types_one.depth = (select (note_types_two.depth+1) as new_depth from notes JOIN note_types AS note_types_two ON notes.note_type_id = note_types_two.id where notes.id = ".$this->db->escape($parentId).");");
         $noteTypeIdRow = $noteTypeIdQuery->row();
@@ -46,6 +47,9 @@ class Notes_admin_model extends CI_Model {
     function editContent($id,$editedContent)
     {
         // TODO error handling
+        //TODO: The line below should NOT be here. Dunno why it is required.
+        $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
+
         $this->db->query("UPDATE notes SET content=".$this->db->escape($editedContent)." WHERE id = $id;");
         return true;
     }
@@ -54,6 +58,8 @@ class Notes_admin_model extends CI_Model {
     function deleteContent($id)
     {
         // TODO error handling and safety restrictions?
+        //TODO: The line below should NOT be here. Dunno why it is required.
+        $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
         $this->db->query("DELETE FROM notes WHERE id = $id;");
         return true;
     }
@@ -62,6 +68,8 @@ class Notes_admin_model extends CI_Model {
     function addTag($parentId, $newContent)
     {
         // TODO error handling and safety restrictions?
+        //TODO: The line below should NOT be here. Dunno why it is required.
+        $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
         $this->db->query("INSERT INTO tags(notes_id,content) VALUES (".$this->db->escape($parentId).",".$this->db->escape($newContent).");");
         return true;
     }
@@ -70,6 +78,8 @@ class Notes_admin_model extends CI_Model {
     function deleteTag($id)
     {
         // TODO error handling and safety restrictions?
+        //TODO: The line below should NOT be here. Dunno why it is required.
+        $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
         $this->db->query("DELETE FROM tags WHERE id = ".$this->db->escape($id).";");
         return true;
     }
@@ -78,6 +88,8 @@ class Notes_admin_model extends CI_Model {
     function addMedia($parentId, $newContent, $type, $description)
     {
         // TODO error handling and safety restrictions?
+        //TODO: The line below should NOT be here. Dunno why it is required.
+        $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
         if($description == NULL) {
             $this->db->query("INSERT INTO media (notes_id,content,description,media_type_id) VALUES (".$this->db->escape($parentId).",".$this->db->escape($newContent).", NULL, (SELECT id FROM media_types WHERE type = ".$this->db->escape($type)."));");
         } else {
@@ -90,6 +102,8 @@ class Notes_admin_model extends CI_Model {
     function deleteMedia($id)
     {
         // TODO error handling and safety restrictions?
+        //TODO: The line below should NOT be here. Dunno why it is required.
+        $this->db->query("START TRANSACTION WITH CONSISTENT SNAPSHOT ");
         $this->db->query("DELETE FROM media WHERE id = ".$this->db->escape($id).";");
     	return true;
     }
--- src/tz/co/shuledirect/api/application/tests/js/notesRequestTest.js
+++ src/tz/co/shuledirect/api/application/tests/js/notesRequestTest.js
@@ -10,44 +10,19 @@ interactions with the db are NOT tested in this file.
 
 */
 
-
-// IMPORT JQUERY!!!!!!
-var EC2_URL = "ec2-54-200-106-165.us-west-2.compute.amazonaws.com";
-
-function ajaxRequest(methodURL, inputJson, handler) {
+function ajaxRequest(methodURL, inputJson) {
     var eurl = "/api/index.php/" + methodURL;
     var params = 'inputJson='+ inputJson;   
     
     var post = $.ajax({
     	type: "POST",
     	url: eurl,
-    	data: params
+    	data: params,
+        dataType: 'json',
+        async: false
     });
-
-    post.done(function(result) {
-        handler(result);
-    });
-
-    post.fail(function() {
-        handler('error');
-    });
+    return $.parseJSON($.parseJSON(post.responseText)); //dunno why we have to parse this twice
 }
-
-
-asyncTest('getAugmentedNotes test1', function() {
-    var result = 0;
-    var methodURL = "notes/getAugmentedNotes";
-    var inputJson = '{"id": 123}';
-
-    var output = '123';
-
-    ajaxRequest(methodURL,inputJson,function(response) {
-        result = response;
-
-	    equal(result, output, "The result is exactly as expected");
-	    start();
-        
-    });
 
 
 /**
@@ -62,84 +37,66 @@ getAugmented notes of the id
 
 
 */
-asyncTest('addContent, getId and getAugmentedNotes test1', function() {
-
-    //getId of form I
-    ajaxRequest("notes/getId",'{"form": "Form 1"}',function(response) {
-        var formId = response; 
-        equal(response, 2, "The id of form 1 should be 2");
-        start();
-    });
+test('addContent, getId and getAugmentedNotes test1', function() {
+    //getId of form I = 2
+    output = ajaxRequest("notes/getId",'{"form": "Form 1"}');
+    equal(output["id"],2, "The id of form 1 should be 2");
+    var formId = output["id"];
 
     //now we add some subjects
-    var subjectJson1 = '{"parentId":' + formId + ',"content":"Physics"';
-    var subjectJson2 = '{"parentId":' + formId + ',"content":"Chemistry"';
-    var subjectJson3 = '{"parentId":' + formId + ',"content":"History"';
-    ajaxRequest("notesAdmin/addContent", subjectJson1,function(response) {
-        equal(response, true, "added physics as a subject");
-        start();
-    });
+    var subjectJson1 = '{"parentId":' + formId + ',"content":"Physics"}';
+    var subjectJson2 = '{"parentId":' + formId + ',"content":"Chemistry"}';
+    var subjectJson3 = '{"parentId":' + formId + ',"content":"History"}';
+    output = ajaxRequest("notesAdmin/addContent", subjectJson1);
+    equal(output, true, "added physics as a subject");
 
-    ajaxRequest("notesAdmin/addContent", subjectJson2,function(response) {
-        equal(response, true, "added chemistry as a subject");
-        start();
-    });
+    output = ajaxRequest("notesAdmin/addContent", subjectJson2);
+    equal(output, true, "added chemistry as a subject");
 
-    ajaxRequest("notesAdmin/addContent", subjectJson3,function(response) {
-        equal(response, true, "added history as a subject");
-        start();
-    });
+    output = ajaxRequest("notesAdmin/addContent", subjectJson3);
+    equal(output, true, "added history as a subject");
+
 
     //we call getId of physics that we just added
-    ajaxRequest("notes/getId",'{"form": "Form 1", "subject": "Physics"}',function(response) {
-        var subjectId = response; 
-        ok(response, "The request went through");
-        start();
-    });
+    output = ajaxRequest("notes/getId",'{"form": "Form 1", "subject": "Physics"}');
+    ok(output["id"],"The request went through");
+    var subjectId = output["id"];
 
     //add some topics to physics
-    var topicJson1 = '{"parentId":' + subjectId + ',"content":"Mechanics"';
-    var topicJson2 = '{"parentId":' + subjectId + ',"content":"Electricity"';
+    var topicJson1 = '{"parentId":' + subjectId + ',"content":"Mechanics"}';
+    var topicJson2 = '{"parentId":' + subjectId + ',"content":"Electricity"}';
 
-    ajaxRequest("notesAdmin/addContent", topicJson1,function(response) {
-        equal(response, true, "added mechanics as a topic");
-        start();
-    });
+    output = ajaxRequest("notesAdmin/addContent", topicJson1);
+    equal(output, true, "added mechanics as a topic");
 
-    ajaxRequest("notesAdmin/addContent", topicJson2,function(response) {
-        equal(response, true, "added electricity as a topic");
-        start();
-    });
+    output = ajaxRequest("notesAdmin/addContent", topicJson2);
+    equal(output, true, "added electricity as a topic");
+
 
     //we call getId of Mechanics that we just added
-    ajaxRequest("notes/getId",'{"form": "Form 1", "subject": "Physics", "topic":"Mechanics"}',function(response) {
-        var topicId = response; 
-        ok(response, "The request went through");
-        start();
-    });    
+    output = ajaxRequest("notes/getId",'{"form": "Form 1", "subject": "Physics", "topic":"Mechanics"}');
+    ok(output["id"],"The request went through");
+    var topicId = output["id"];
+    alert(topicId);
 
     //add some subTopics to physics
-    var subTopicJson1 = '{"parentId":' + topicId + ',"content":"Force"';
-    var subTopicJson2 = '{"parentId":' + topicId + ',"content":"Kinematics"';
+    var subTopicJson1 = '{"parentId":' + topicId + ',"content":"Force"}';
+    var subTopicJson2 = '{"parentId":' + topicId + ',"content":"Kinematics"}';
 
-    ajaxRequest("notesAdmin/addContent", subTopicJson1,function(response) {
-        equal(response, true, "added Force as a subtopic to mechanics");
-        start();
-    });
 
-    ajaxRequest("notesAdmin/addContent", subTopicJson2,function(response) {
-        equal(response, true, "added kinematics as a subtopic to mechanics");
-        start();
-    });
+    output = ajaxRequest("notesAdmin/addContent", subTopicJson1);
+    equal(output, true, "added mechanics as a topic");
+
+    output = ajaxRequest("notesAdmin/addContent", subTopicJson2);
+    equal(output, true, "added electricity as a topic");
 
     //now we getAugmentedNotes of the physics subject that we just put in
     var subjectIdJson = '{"id":' + subjectId +'}';
-    ajaxRequest("notes/getAugmentedNotes", subjectIdJson,function(response) {
-        ok(response, "getting notes from physics");
-        start();
-    });
+    output = ajaxRequest("notes/getAugmentedNotes", subjectIdJson);
+    equal(output, true, "This is the easiest way to print my response lol");
+    var augmentedNotes = output;
 
-    //parse the Augmented Notes to find what we want
-    //TODO do this
+    // //parse the Augmented Notes to find what we want
+    // //TODO do this
 
 });
